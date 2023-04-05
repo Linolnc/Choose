ef8df43492f8698cf43c61953516be8e2ab303e0
Lincoln Baker <lincbake795@ed.amdsb.ca>
2023-04-05 14:36:50 -0400

    Add project files.

---
+++ Choose/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Choose
+{
+    partial class optionOne
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(optionOne));
+            this.opt = new System.Windows.Forms.Button();
+            this.optiontwo = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.Label();
+            this.hud = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.hud)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // opt
+            // 
+            this.opt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.opt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opt.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.opt.Location = new System.Drawing.Point(71, 341);
+            this.opt.Name = "opt";
+            this.opt.Size = new System.Drawing.Size(146, 49);
+            this.opt.TabIndex = 0;
+            this.opt.Text = "optionOne";
+            this.opt.UseVisualStyleBackColor = true;
+            this.opt.Click += new System.EventHandler(this.opt_Click);
+            // 
+            // optiontwo
+            // 
+            this.optiontwo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.optiontwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optiontwo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.optiontwo.Location = new System.Drawing.Point(543, 341);
+            this.optiontwo.Name = "optiontwo";
+            this.optiontwo.Size = new System.Drawing.Size(142, 49);
+            this.optiontwo.TabIndex = 1;
+            this.optiontwo.Text = "optionTwo";
+            this.optiontwo.UseVisualStyleBackColor = true;
+            this.optiontwo.Click += new System.EventHandler(this.optiontwo_Click);
+            // 
+            // output
+            // 
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.output.Location = new System.Drawing.Point(157, 245);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(398, 83);
+            this.output.TabIndex = 2;
+            this.output.Text = "output";
+            this.output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hud
+            // 
+            this.hud.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.hud.Location = new System.Drawing.Point(127, 3);
+            this.hud.Name = "hud";
+            this.hud.Size = new System.Drawing.Size(461, 239);
+            this.hud.TabIndex = 3;
+            this.hud.TabStop = false;
+            // 
+            // optionOne
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hud);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.optiontwo);
+            this.Controls.Add(this.opt);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "optionOne";
+            this.Text = "Wacky Adventure. ";
+            this.Load += new System.EventHandler(this.optionOne_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.hud)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button opt;
+        private System.Windows.Forms.Button optiontwo;
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.PictureBox hud;
+    }
+}
+
---
+++ Choose/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Choose
+{
+    public partial class optionOne : Form
+    {
+        int page = 1;
+
+        Random randGen = new Random();
+
+        public optionOne()
+        {
+            InitializeComponent();
+            output.Text = "Start the game?";
+            opt.Text = "No";
+            optiontwo.Text = "Yes";
+            hud.BackgroundImage = Properties.Resources.babaya;
+        }
+
+        private void optiontwo_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 3;
+            }
+            else if (page == 4)
+            {
+                page = 6;
+            }
+            else if (page == 7)
+            {
+                page = 8;
+            }
+            else if (page == 8)
+            {
+                page = 100;
+            }
+            else if (page == 9)
+            {
+                page = 1;
+            }
+            else if (page == 3)
+            {
+                page = 100;
+            }
+            else if (page == 6)
+            {
+                page = 100; 
+            }
+            else if (page == 99)
+            {
+                page = 100; 
+            }
+            else if (page == 9)
+            {
+                page = 100;
+            }
+            switch (page)
+            {
+                case 1:
+                    output.Text = "Start?";
+
+                    opt.Text = "No";
+                    optiontwo.Text = "Yes";
+                    hud.BackgroundImage = Properties.Resources.babaya;
+
+                    break;
+                case 2:
+                    output.Text = "You wake up in a strange town, the cars are all little tike cars. What will be your next action?";
+
+                    opt.Text = "Walk down the road";
+                    optiontwo.Text = "Ask where you are";
+
+                    hud.BackgroundImage = Properties.Resources.IMG_4040;
+                    break;
+                case 4:
+                    output.Text = "You walk towards a massive pyramid, guarded by a few bodyguards";
+
+                    opt.Text = "FIGHT EM";
+                    optiontwo.Text = "Sneak in";
+                    hud.BackgroundImage = Properties.Resources.B169944B_01E6_4579_B80B_664C1A1835E8;
+                    break;
+                case 3:
+                    output.Text = "You walk up to the Baby Man, He starts to scream and cry so loud you die";
+
+                    opt.Text = "Reset";
+                    optiontwo.Text = "Don't";
+                    hud.BackgroundImage = Properties.Resources.hqdefault;
+                    break;
+                case 6:
+                    output.Text = "You sneak too loudly are are caught, you get thrown in to Time Out until you ROT";
+
+                    opt.Text = "Reset";
+                    optiontwo.Text = "Dont";
+                    hud.BackgroundImage = Properties.Resources.hqdefault;
+                    break;
+                case 7:
+                    output.Text = "You fight your way through the gaurds and enter the pyramid, now face to face with K I N G...";
+
+                    optiontwo.Text = "Establish Peace";
+                    opt.Text = "Assasinate him";
+                    hud.BackgroundImage = Properties.Resources.IMG_4046; 
+                    break;
+                case 8:
+                    output.Text = "You approach the king with a peace treaty and become his friend, good job you won and you aren't a monster!";
+
+                    opt.Text = "RESTART";
+                    optiontwo.Text = "end";
+                    hud.BackgroundImage = Properties.Resources.WINER; 
+                    break;
+                   
+                case 99:
+                    output.Text = "LEAVE NOW!!";
+
+                    opt.Text = "RESTART";
+                    optiontwo.Text = "end";
+                    hud.BackgroundImage = Properties.Resources.hqdefault;
+                    break;
+                case 100:
+                    output.Text = "Vanish";
+                    opt.Text = "no turning back";
+                    optiontwo.Text = "no turning back";
+                    hud.BackgroundImage = Properties.Resources.bi45zvq82wg71;
+
+                    break;
+                case 9:
+                    output.Text = "You approach the king and assasinate him and take his place as ruler, you won but you're also a monster";
+
+                    opt.Text = "RESTART";
+                    optiontwo.Text = "end";
+                    hud.BackgroundImage = Properties.Resources.WINER;
+
+                    break;
+            }
+        }
+        private void opt_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 99;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if (page == 4)
+            {
+                page = 7;
+            }
+            else if (page == 7)
+            {
+                page = 9; 
+            }
+            else if (page == 3)
+            {
+                page = 1; 
+            }
+            else if (page == 9)
+            {
+                page = 1;
+            }
+            else if (page == 6)
+            {
+                page = 1;
+            }
+            else if (page == 99)
+            {
+                page = 1; 
+            }
+            else if (page == 8)
+            {
+                page = 1; 
+            }
+              
+
+                switch (page)
+                {
+                    case 1:
+                        output.Text = "Start?";
+
+                        opt.Text = "No";
+                        optiontwo.Text = "Yes";
+                    hud.BackgroundImage = Properties.Resources.babaya;
+
+                    break;
+                    case 2:
+                        output.Text = "You wake up in a strange town, the cars are all little tike cars. What will be your next action?";
+
+                        opt.Text = "Walk down the road";
+                        optiontwo.Text = "Ask where you are";
+
+                        ;
+                        break;
+                    case 4:
+                        output.Text = "You walk towards a massive pyramid, guarded by a few bodyguards";
+
+                        opt.Text = "FIGHT EM";
+                        optiontwo.Text = "Sneak in";
+                    hud.BackgroundImage = Properties.Resources.B169944B_01E6_4579_B80B_664C1A1835E8;
+                    break;
+                    case 3:
+                        output.Text = "You walk up to the Baby Man, He starts to scream and cry so loud you die";
+
+                        opt.Text = "Reset";
+                        optiontwo.Text = "Don't";
+                        break;
+                    case 6:
+                        output.Text = "You sneak too loudly are are caught, you get thrown in to Time Out until you ROT";
+
+                        opt.Text = "Reset";
+                        optiontwo.Text = "Dont";
+                        break;
+                    case 7:
+                        output.Text = "You fight your way through the gaurds and enter the pyramid, now face to face with K I N G...";
+
+                        optiontwo.Text = "Establish Peace";
+                        opt.Text = "Assasinate him";
+                    hud.BackgroundImage = Properties.Resources.IMG_4046; 
+                    break;
+                    case 8:
+                        output.Text = "You approach the king with a peace treaty and become his friend, good job you won and you aren't a monster!";
+
+                        opt.Text = "RESTART";
+                        optiontwo.Text = "end";
+                    hud.BackgroundImage = Properties.Resources.WINER; 
+                    break;
+                    case 99:
+                        output.Text = "LEAVE NOW!!";
+
+                        opt.Text = "RESTART";
+                        optiontwo.Text = "end";
+                        break;
+                    case 100:
+                        output.Text = "Vanish";
+                        opt.Text = "no turning back";
+                        optiontwo.Text = "no turning back";
+                    hud.BackgroundImage = Properties.Resources.bi45zvq82wg71; 
+                    break;
+                    case 9:
+                        output.Text = "You approach the king and assasinate him and take his place as ruler, you won but you're also a monster";
+
+                        opt.Text = "RESTART";
+                        optiontwo.Text = "end";
+                    hud.BackgroundImage = Properties.Resources.WINER;
+
+
+                        break;
+                }
+            }
+
+        private void optionOne_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_ChangeUICues(object sender, UICuesEventArgs e)
+        {
+
+        }
+    }
+    }
+
+
+
